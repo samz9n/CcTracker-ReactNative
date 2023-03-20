be7108ef6db1140f81d4cbfbc39c552a7da7b0eb
samz9n <samuel.langhoff@outlook.com>
2023-03-20 12:01:24 +0200

    added styling to bottom navigation

--- App.tsx
+++ App.tsx
@@ -1,16 +1,30 @@
-/* import { StatusBar } from 'expo-status-bar'; */
 import { StyleSheet, Text, View } from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
 import News from './screens/News';
 import Watchlist from './screens/Watchlist';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 const Tab = createBottomTabNavigator();
 
 export default function App() {
+	let iconName: string;
 	return (
 		<NavigationContainer>
-			<Tab.Navigator>
+			<Tab.Navigator
+				screenOptions={({ route }) => ({
+					tabBarIcon: ({ focused, color, size }) => {
+						if (route.name === 'News') {
+							iconName = focused ? 'ios-newspaper' : 'ios-newspaper-outline';
+						} else if (route.name === 'Watchlist') {
+							iconName = focused ? 'md-star-sharp' : 'md-star-outline';
+						}
+						return <Ionicons name={iconName} size={size} color={color} />;
+					},
+					tabBarActiveTintColor: 'tomato',
+					tabBarInactiveTintColor: 'gray'
+				})}
+			>
 				<Tab.Screen name="Watchlist" component={Watchlist} />
 				<Tab.Screen name="News" component={News} />
 			</Tab.Navigator>
