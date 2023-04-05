bfe82c532be6d8ca7fccf6ff72b304db78ce2eb7
samz9n <samuel.langhoff@outlook.com>
2023-04-05 11:12:21 +0300

    Working on coin modal, now shows the right coin when open

--- components/ui/CoinModal.tsx
+++ components/ui/CoinModal.tsx
@@ -1,12 +1,68 @@
-import { StyleSheet, Text, View } from 'react-native';
-import React from 'react';
+import React, { useState } from 'react'
+import {
+  StyleSheet,
+  Modal,
+  Text,
+  View,
+  ScrollView,
+  TouchableOpacity,
+} from 'react-native'
+import GestureRecognizer from 'react-native-swipe-gestures'
+import Ionicons from 'react-native-vector-icons/Ionicons'
+import { Coin } from '../../types/types'
 
-export default function CoinModal() {
-	return (
-		<View>
-			<Text>CoinModal</Text>
-		</View>
-	);
+interface ModalProps {
+  isVisible: boolean
+  closeModal: () => void
+  coin: Coin | null
 }
 
-const styles = StyleSheet.create({});
+const CoinModal = ({ isVisible, closeModal, coin }: ModalProps) => {
+  return (
+    <GestureRecognizer style={{ flex: 1 }} onSwipeDown={closeModal}>
+      <Modal
+        animationType='slide'
+        transparent={true}
+        visible={isVisible}
+        onRequestClose={closeModal}
+      >
+        <View style={{ flex: 1, justifyContent: 'flex-end' }}>
+          <View style={styles.modalContainer}>
+            <TouchableOpacity style={styles.closeBtn} onPress={closeModal}>
+              <Ionicons name='close' size={25} color='white' />
+            </TouchableOpacity>
+            <ScrollView>
+              <Text style={styles.coinTxt}>Name: {coin?.name}</Text>
+              <Text style={styles.coinTxt}>Symbol: {coin?.symbol}</Text>
+              <Text style={styles.coinTxt}>Price: {coin?.price}</Text>
+            </ScrollView>
+          </View>
+        </View>
+      </Modal>
+    </GestureRecognizer>
+  )
+}
+
+const styles = StyleSheet.create({
+  modalContainer: {
+    backgroundColor: 'rgb(14, 1, 41)',
+    height: '82%',
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,
+    borderTopWidth: 1,
+    borderColor: 'rgb(190, 92, 11)',
+    paddingHorizontal: 20,
+    paddingTop: 20,
+  },
+  closeBtn: {
+    position: 'absolute',
+    top: 8,
+    right: 8,
+  },
+  coinTxt: {
+    fontSize: 20,
+    color: 'white',
+  },
+})
+
+export default CoinModal
--- screens/Search.tsx
+++ screens/Search.tsx
@@ -6,12 +6,15 @@ import ListSeparator from '../components/ui/ListSeparator'
 import LoadingSign from '../components/ui/LoadingSign'
 import Searchbar from '../components/ui/Searchbar'
 import { Coin } from '../types/types'
+import CoinModal from '../components/ui/CoinModal'
 
 export default function Search() {
   const [top100, setTop100] = useState<Coin[]>([])
   const [filteredCoinList, setFilteredCoinList] = useState<Coin[]>([])
   const [searchText, setSearchText] = useState('')
   const [isLoading, setIsLoading] = useState(false)
+  const [isModalVisible, setIsModalVisible] = useState(false)
+  const [selectedCoin, setSelectedCoin] = useState<Coin | null>(null)
 
   /* We fetch the top 100 coins and set them to state so we can map them later on */
   useEffect(() => {
@@ -43,10 +46,20 @@ export default function Search() {
     }
   }
 
+  function closeModal() {
+    setIsModalVisible(false)
+    setSelectedCoin(null)
+  }
+
   if (isLoading) return <LoadingSign message='Fetching coins...' />
 
   return (
     <View style={styles.container}>
+      <CoinModal
+        isVisible={isModalVisible}
+        closeModal={closeModal}
+        coin={selectedCoin}
+      ></CoinModal>
       <Searchbar
         onChangeText={(text) => searchFunction(text)}
         value={searchText}
@@ -57,7 +70,10 @@ export default function Search() {
         renderItem={({ item }) => {
           return (
             <CoinListButton
-              onPress={() => {}}
+              onPress={() => {
+                setSelectedCoin(item)
+                setIsModalVisible(true)
+              }}
               symbol={item.symbol}
               name={item.name}
               price={item.price}
