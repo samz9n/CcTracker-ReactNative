ba4fa145f83a362fb11d85632b707eed734659a3
samz9n <samuel.langhoff@outlook.com>
2023-04-11 15:43:58 +0300

    asyncstorage works now and saves the watchlist for next login

--- store/watchlist-context.tsx
+++ store/watchlist-context.tsx
@@ -1,5 +1,6 @@
-import { createContext, ReactNode, useState } from 'react'
+import { createContext, ReactNode, useState, useEffect } from 'react'
 import { Coin } from '../types/types'
+import AsyncStorage from '@react-native-async-storage/async-storage'
 
 interface WatchlistContextProps {
   watchlist: Coin[]
@@ -23,6 +24,33 @@ export default function WatchlistContextProvider({
   children,
 }: WatchlistContextProviderProps) {
   const [watchlist, setWatchlist] = useState<Coin[]>([])
+
+  useEffect(() => {
+    /* We load the watchlist from AsyncStorage when the app starts, to get users favorite coins */
+    async function loadWatchlist() {
+      try {
+        const watchlistJSON = await AsyncStorage.getItem('@watchlist')
+        if (watchlistJSON !== null) {
+          setWatchlist(JSON.parse(watchlistJSON))
+        }
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    loadWatchlist()
+  }, [])
+
+  useEffect(() => {
+    /* On every change to the watchlist, we save it to AsyncStorage */
+    async function saveWatchlist() {
+      try {
+        await AsyncStorage.setItem('@watchlist', JSON.stringify(watchlist))
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    saveWatchlist()
+  }, [watchlist])
 
   function addToWatchlist(coin: Coin) {
     setWatchlist((prevWatchlist) => {
