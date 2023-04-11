5a616b8fe5087c8dcc28a1038667894cd52ca306
samz9n <samuel.langhoff@outlook.com>
2023-04-11 10:53:11 +0300

    added asyncStorage and made searchbar text darker

--- components/ui/Searchbar.tsx
+++ components/ui/Searchbar.tsx
@@ -1,24 +1,28 @@
-import { StyleSheet } from 'react-native';
-import React from 'react';
-import { SearchBar } from '@rneui/themed';
+import { StyleSheet } from 'react-native'
+import React from 'react'
+import { SearchBar } from '@rneui/themed'
 
 interface SearchbarProps {
-	onChangeText: (text: string) => void;
-	value: string;
+  onChangeText: (text: string) => void
+  value: string
 }
 
 export default function Searchbar({ onChangeText, value }: SearchbarProps) {
-	return (
-		<SearchBar
-			inputContainerStyle={{ backgroundColor: 'rgb(210, 209, 211)', borderRadius: 10, height: 35 }}
-			containerStyle={{ backgroundColor: 'rgb(13, 0, 24)' }}
-			/* inputStyle={{ color: 'white' }} */
-			placeholder="Search Here..."
-			onChangeText={onChangeText}
-			autoCorrect={false}
-			value={value}
-		/>
-	);
+  return (
+    <SearchBar
+      inputContainerStyle={{
+        backgroundColor: 'rgb(210, 209, 211)',
+        borderRadius: 10,
+        height: 35,
+      }}
+      containerStyle={{ backgroundColor: 'rgb(13, 0, 24)' }}
+      inputStyle={{ color: 'black' }}
+      placeholder='Search Here...'
+      onChangeText={onChangeText}
+      autoCorrect={false}
+      value={value}
+    />
+  )
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({})
